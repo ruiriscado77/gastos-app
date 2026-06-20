5d59404853ba79c3751a9fa55fafe8eb411c37b7
ruiriscado77 <ruiriscado@gmail.com>
2026-06-20 15:58:22 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+{
+  "name": "Gastos Rui & Cláudia",
+  "short_name": "Gastos",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#F7F8FC",
+  "theme_color": "#ffffff",
+  "icons": [
+    {
+      "src": "/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
